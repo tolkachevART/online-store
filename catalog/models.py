9801f81d5e5df4be8242ed0ec920b598f29dd3f6
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-08-16 21:39:30 +0300

    TASK 2,3 added fields for models Category, Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование категории")
+    description = models.TextField(verbose_name="Описание категории")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование товара")
+    description = models.TextField(verbose_name="Описание товара")
+    image = models.ImageField(
+        upload_to="product/img", verbose_name="Превью товара", **NULLABLE
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        **NULLABLE,
+        related_name="products"
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["name", "category"]
