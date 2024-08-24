86eb228fe45bbb19092fabb5e2ae81c32731f595
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-08-24 06:59:02 +0300

    Task 2  created template for the main page(list of products)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, base
+from catalog.views import home, contacts, products_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
     path("contacts/", contacts, name="contacts"),
-    path("base/", base, name="base"),
+    path('base/', products_list, name='products')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
@@ -10,5 +12,7 @@ def contacts(request):
     return render(request, "contacts.html")
 
 
-def base(request):
-    return render(request, "base.html", {"base": base})
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'base.html', context)
