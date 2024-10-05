9902591cb1b2140b02f531a7000048eae348c952
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-10-05 10:25:55 +0300

    Task 1 install redis

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,12 @@ EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://localhost:6379'
+        }
+    }
